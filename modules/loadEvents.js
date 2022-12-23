2d7f74f233c317cd5abd036aaeee1a9c5b622165
Ceed#2117 <82039844+ceedledev@users.noreply.github.com>
2022-12-23 10:30:26 +0100

    Update loadEvents.js

--- modules/loadEvents.js
+++ modules/loadEvents.js
@@ -4,7 +4,7 @@ module.exports = async bot => {
 
    fs.readdirSync("./events").filter(f => f.endsWith(".js")).forEach(async file => {
    
-      let event = require(`../Events/${file}`)
+      let event = require(`../events/${file}`)
       bot.on(file.split(".js").join(""), event.bind(null, bot))
       console.log(`Event: ${file} load!`)
    })
