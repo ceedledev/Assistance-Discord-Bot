41a333b242a9439eb1855bf3f4185ca2cec20ae5
Ceed#2117 <82039844+ceedledev@users.noreply.github.com>
2022-12-23 09:45:40 +0100

    Update main.js

--- main.js
+++ main.js
@@ -2,18 +2,11 @@ const Discord = require("discord.js")
 const intents = new Discord.IntentsBitField(3276799)
 const bot = new Discord.Client({intents})
 const loadCommands = require("./Loaders/loadCommands")
+const loadEvents = require("./Loaders/loadEvents")
 const config = require("./config.js")
 
 bot.commands = new Discord.Collection()
 
 bot.login(config.token)
 loadCommands(bot)
-
-bot.on("messageCreate", async message => {
-
-if(message.content === "!ping") return bot.commands.get("ping").run(bot, message)
-})
-
-bot.on("ready", async () => {
-console.log(`${bot.user.tag} and online!`)
-})
+loadEvents(bot)
