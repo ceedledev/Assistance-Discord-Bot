abb768b2dc12315fb43a12652d7610af032a3fb8
Ceed#2117 <82039844+ceedledev@users.noreply.github.com>
2022-12-23 10:29:44 +0100

    Update loadCommands.js

--- modules/loadCommands.js
+++ modules/loadCommands.js
@@ -4,7 +4,7 @@ module.exports = async bot => {
 
    fs.readdirSync("./commands").filter(f => f.endsWith(".js")).forEach(async file => {
    
-      let command = require(`../Commandes/${file}`)
+      let command = require(`../commands/${file}`)
       if(!command.name || typeof command.name !== "string") throw new TypeError(`The command ${file.slice(0, file.length - 3)} has no name`)
       bot.commands.set(command.name, command)
       console.log(`Command: ${file} loaded successfully!`)
