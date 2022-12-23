fa9747253365abdf45bec41d6aa8a69f0263e03c
Ceed#2117 <82039844+ceedledev@users.noreply.github.com>
2022-12-23 10:44:54 +0100

    Create interactionCreate.js

---
+++ events/interactionCreate.js
@@ -0,0 +1,10 @@
+const Discord = require("discord.js")
+
+module.exports = async (bot, interaction) => {
+
+if(interaction.type === Discord.InteractionType.ApplicationCommand) {
+
+   let command = require(`../commands/${interaction.commandName}`)
+   command.run(bot, interaction, command.options)
+   }
+}
